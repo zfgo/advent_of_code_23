4cc5b8b1f743865338f7fec2c9aab64af2150572
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-12-13 20:39:52 -0800

    initial commit

---
+++ 08/day_08-1.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define WORD_LENGTH 16
+#define hashsize(n) ((unsigned long)1 << (n))
+#define hashmask(n) (hashsize(n) - 1)
+
+char *read_line(int size)
+{
+    char *str;
+    int ch;
+    int len = 0;
+
+    str = (char *)malloc(size);
+    if (str == NULL)
+    {
+        fprintf(stderr, "Malloc error, exiting\n");
+        exit(1);
+    }
+
+    while ((ch = getchar()) != EOF && (ch != '\n'))
+    {
+        str[len++] = ch;
+        if (len == size)
+        {
+            size = 2 * size;
+            str = realloc(str, size);
+            if (str == NULL)
+            {
+                fprintf(stderr, "Malloc error, exiting\n");
+                exit(1);
+            }
+        }
+    }
+    str[len] = '\0';
+    return str;
+}
+
+unsigned long oaat(char *key, unsigned long len, unsigned long bits) 
+{
+    unsigned long hash, i;
+
+    for (hash = 0, i = 0; i < len; i++) 
+    {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+
+    return hash & hashmask(bits);
+}
+
+int main()
+{
+    char *line;
+    char *lines[1024];
+    int n = 0;
+
+    line = read_line(WORD_LENGTH);
+
+    while (*line) // line != NULL
+    {
+        lines[n] = line;
+        line = read_line(WORD_LENGTH);
+        ++n;
+    }
+
+    return 0;
+}
